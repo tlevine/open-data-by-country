d9eca0fa9f05803fae268e8badc943889d13b575
Thomas Levine <_@thomaslevine.com>
2014-02-23 04:26:26 +0000

    aoeu

--- analyze.r
+++ analyze.r
@@ -1,6 +1,13 @@
-if (!all(c('cpi','gdp','odi','pop') %in% ls())) {
+library(WDI)
+library(reshape2)
+
+if (!all(c('cpi','gdp','odi','pop','wdi') %in% ls())) {
   cpi <- read.csv('data/cpi.csv')
   gdp <- read.csv('data/gdp.csv')
-  odi <- read.csv('data/open-data-index.csv')
+  odi <- read.csv('data/open-data-index.csv', stringsAsFactors = FALSE)
   pop <- read.csv('data/population.csv')
+  wdi <- list()
+  for (i in WDIsearch()[,'indicator']) {
+    wdi[i] <- WDI(indicator=i, start=2013, end=2013)
+  }
 }
