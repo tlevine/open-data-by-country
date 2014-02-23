cf970f8e9a96d59450d5cad595999b19e16edff6
Thomas Levine <_@thomaslevine.com>
2014-02-23 04:48:30 +0000

    aoeu

--- analyze.r
+++ analyze.r
@@ -1,4 +1,5 @@
 library(WDI)
+library(plyr)
 library(reshape2)
 
 if (!all(c('cpi','gdp','odi','pop','wdi') %in% ls())) {
@@ -10,4 +11,17 @@ if (!all(c('cpi','gdp','odi','pop','wdi') %in% ls())) {
   for (i in WDIsearch()[,'indicator']) {
     wdi[i] <- WDI(indicator=i, start=2013, end=2013)
   }
+}
+names(cpi)[4] <- 'cpi'
+names(gdp)[4] <- 'gdp'
+names(pop)[4] <- 'pop'
+
+odi.country <- dcast(odi, Census.Country ~ Data.Availability..Does.the.data.exist.., value.var = 'Census.Country.1')[c(1,4)]
+names(odi.country) <- c('Country.Name','n.datasets')
+.r <- function(a,b) {
+  join(a, b, by = c('Country.Name','Year'))
+}
+d <- join(.r(.r(cpi, gdp), pop), odi.country, type = 'inner', by = 'Country.Name')
+while ('Country.Code' %in% names(d)) {
+  d$Country.Code <- NULL
 }
